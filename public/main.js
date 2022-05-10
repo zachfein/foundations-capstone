a7cced9e0d84642ea64f8d72f6a5118a38c1df75
zachfein <zvf0307@gmail.com>
2022-05-10 01:17:35 -0600

    finished project

--- public/main.js
+++ public/main.js
@@ -10,19 +10,19 @@ document.getElementById("view-teams-btn").onclick = () => {
 
 const getTeamRoster = (team) => {
   axios.get(`${baseURL}/${team.id}/roster`).then((res) => {
-    displayPlayers(res.data.roster);
+    displayPlayers(res.data.roster, team.name);
   });
 };
 
 const createPlayerDisplay = (player) => {
   const playerDisplay = document.createElement("div");
   playerDisplay.classList.add("player");
-  const playersContainer = document.getElementById("players-container");
+  const playerDiv = document.getElementById("player-div");
   playerDisplay.innerHTML = `
   <p class="player-name">${player.person.fullName}</p>
   <p class="player-number">${player.jerseyNumber}</p>
   <p class="player-position">${player.position.name}</p>`;
-  playersContainer.appendChild(playerDisplay);
+  playerDiv.appendChild(playerDisplay);
 };
 
 const createTeamsDisplay = (team) => {
@@ -32,6 +32,7 @@ const createTeamsDisplay = (team) => {
   teamDisplay.innerHTML = `<p class="team-name">--${team.name}--</p>
   <button class="view-roster" id="${team.abbreviation}">View Roster</button>`;
   teamsContainer.appendChild(teamDisplay);
+  teamsContainer.classList.add("container-border");
   const rosterBtn = document.getElementById(team.abbreviation);
   rosterBtn.addEventListener("click", () => {
     getTeamRoster(team);
@@ -39,12 +40,23 @@ const createTeamsDisplay = (team) => {
 };
 
 const displayTeams = (arr) => {
+  const teamsContainer = document.getElementById("teams-container");
+  teamsContainer.innerHTML = ``;
+  teamsContainer.classList.remove("container-border");
   for (let i = 0; i < arr.length; i++) {
     createTeamsDisplay(arr[i]);
   }
 };
 
-const displayPlayers = (arr) => {
+const displayPlayers = (arr, name) => {
+  const playersContainer = document.getElementById("players-container");
+  playersContainer.innerHTML = ``;
+  let teamName = document.createElement("h1");
+  teamName.textContent = name;
+  const playerDiv = document.createElement("div");
+  playerDiv.setAttribute("id", "player-div");
+  playersContainer.appendChild(teamName);
+  playersContainer.appendChild(playerDiv);
   for (let i = 0; i < arr.length; i++) {
     createPlayerDisplay(arr[i]);
   }
